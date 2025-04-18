ef110174d1724259acbbd8c18240770c861dbfcc
likanug <likanug.g@qq.com>
2025-04-18 18:49:43 +0800

    添加Question模型

---
+++ ch_02/ch02/src/main.rs
@@ -0,0 +1,51 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            true => Err(
+                Error::new(ErrorKind::InvalidInput, "No id provide"),
+            ),
+            false => Ok(QuestionId(id.to_string())),
+        }
+    }
+}
+
+impl Question {
+    fn new(id: QuestionId,
+           title: String,
+           content: String,
+           tags: Option<Vec<String>>,
+    ) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+fn main() {
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "What is Rust?".to_string(),
+        "Rust is a systems programming language.".to_string(),
+        Some(vec!["rust".to_string(), "programming".to_string()]),
+    );
+    println!("{:?}", question);
+}
