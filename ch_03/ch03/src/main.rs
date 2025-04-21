3a4fc262c3fe7d0c3f5d2c7a6a4e422b1a1292f4
likanug <likanug.g@qq.com>
2025-04-21 21:59:09 +0800

    添加"question"的API

--- ch_03/ch03/src/main.rs
+++ ch_03/ch03/src/main.rs
@@ -1,10 +1,71 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
 use warp::Filter;
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            true => Err(
+                Error::new(ErrorKind::InvalidInput, "No id provide"),
+            ),
+            false => Ok(QuestionId(id.to_string())),
+        }
+    }
+}
+
+impl Question {
+    fn new(id: QuestionId,
+           title: String,
+           content: String,
+           tags: Option<Vec<String>>,
+    ) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+
+async fn get_question() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provide"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string())),
+    );
+    Ok(warp::reply::json(
+        &question
+    ))
+    //Ok(())
+}
+
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path("hello")
-        .map(|| "Hello, World!".to_string());
-    warp::serve(hello)
+    let get_items = warp::get()
+        .and(warp::path("question"))
+        .and(warp::path::end())
+        .and_then(get_question());
+    let routes = get_items;
+
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
